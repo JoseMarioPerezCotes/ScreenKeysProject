b40569a98bd6979f0f36fcf946e6450721c6f613
JoseMarioPerez <josemario1088@hotmail.com>
2018-01-15 12:20:07 -0500

    Graphics and Script Folder

---
+++ ScreenKeys/Assets/Script/HandleInput.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleInput : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		foreach (KeyCode key in System.Enum.GetValues(typeof(KeyCode))) {
+			if (Input.GetKeyUp (key)) {
+				Debug.Log ("Key Pressed:" + key.ToString ());
+			}
+		}
+	}
+}
